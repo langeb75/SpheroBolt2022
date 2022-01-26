628a2d28a3f18e423f66401cc4dbe0aed155f68e
Brad Lange <blange@gulllakecs.org>
2022-01-26 12:36:21 -0500

    Initial Haunted Mansion Commit

---
+++ HauntedMansion/Haunted Mansion Program/HauntedMansion.js
@@ -0,0 +1,7 @@
+/*
+Programmer: Mr. Lange
+Team Member: Mr. Zelda
+Date: 1.26.2022
+Program: We will be making a program to go through a Haunted Mansion Maze filled with Haunted 
+Sounds, Changing of Colors, and other obstacles along the way using a Sphero Bolt and Java Script
+*/
